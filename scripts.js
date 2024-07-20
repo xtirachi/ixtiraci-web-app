9f5509c670c9825b60fa7fecc07e732aaa2a9637
xtirachi <168432733+xtirachi@users.noreply.github.com>
2024-07-20 12:27:03 +0400

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -68,7 +68,7 @@ function uploadFile(event) {
     formData.append('part', selectedPart);
 
     // Example endpoint for handling file upload
-    fetch('https://example.com/upload', {
+    fetch('https://script.google.com/macros/s/AKfycbzRXEEukOZ8nt_8SlYpxgWu1CX__WxZHhwbk5uYGmZvy-GV8sTBUIS_f1Z-YNe7RnrXEg/exec', {
         method: 'POST',
         body: formData
     }).then(response => response.json())
