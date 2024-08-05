94909bed2cf03c743fd9156bf7dd772c0d404b35
xtirachi <168432733+xtirachi@users.noreply.github.com>
2024-08-05 11:35:20 +0400

    Update script.js

--- script.js
+++ script.js
@@ -9,6 +9,17 @@ function promptPassword(travelDestination, password) {
     }
 }
 
+
+function promptPassword(country, correctPassword) {
+    const password = prompt("Zəhmət olmasa " + country + " üçün gizli kodu qeyd edin:");
+    if (password === correctPassword) {
+        navigateToLanguageSelection(country);
+    } else {
+        alert("Gizli kodu yanlış qeyd etdiniz. Votsap vasitəsilə bizimlə əlaqə saxlayın!");
+    }
+}
+
+
 // Function to navigate back to the main page
 function navigateToMainPage() {
     // Logic to navigate back to the main page
