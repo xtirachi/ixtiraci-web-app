302b427902f71ec9fa4df4a6a6be4b4391e6e2fa
xtirachi <168432733+xtirachi@users.noreply.github.com>
2024-08-05 11:48:00 +0400

    Update script.js

--- script.js
+++ script.js
@@ -8,6 +8,11 @@ function promptPassword(country, correctPassword) {
     }
 }
 
+function navigateToLanguageSelection(country) {
+    selectedCountry = country;
+    document.getElementById('introduction-page').classList.add('hidden');
+    document.getElementById('language-selection-page').classList.remove('hidden');
+}
 
 // Function to navigate back to the main page
 function navigateToMainPage() {
