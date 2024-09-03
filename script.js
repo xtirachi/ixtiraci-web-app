2552be4b93057f4bef9caf6cc4b1011476b47f05
xtirachi <168432733+xtirachi@users.noreply.github.com>
2024-09-03 15:19:37 +0400

    Update script.js

--- script.js
+++ script.js
@@ -3,12 +3,14 @@ let selectedCountry = '';
 function promptPassword(country, correctPassword) {
     const storedPassword = localStorage.getItem(country);
     if (storedPassword === correctPassword) {
-        navigateToLanguageSelection(country);
+       navigateToCountryOptions(country) {
+    window.location.href = `${country}-options-page-az.html`;
     } else {
         const password = prompt("Zəhmət olmasa " + country + " üçün gizli kodu qeyd edin:");
         if (password === correctPassword) {
             localStorage.setItem(country, correctPassword);
-            navigateToLanguageSelection(country);
+           navigateToCountryOptions(country) {
+    window.location.href = `${country}-options-page-az.html`;
         } else {
             alert("Gizli kodu yanlış qeyd etdiniz. Votsap vasitəsilə bizimlə əlaqə saxlayın!");
         }
